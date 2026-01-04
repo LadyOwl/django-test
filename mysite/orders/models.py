342d96979fc37886d386b0601c65591eec888113
Юлия (Джулия) <sixquestions666@gmail.com>
2026-01-04 13:47:57 +0300

    Update Users and Orders

--- mysite/orders/models.py
+++ mysite/orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import User
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models. CASCADE, related_name='orders')
+    product = models.CharField(max_length=30)
+    price = models.IntegerField()
 # Create your models here.
